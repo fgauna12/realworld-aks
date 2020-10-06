8fc7c477727e5579ff052086de75c9529e052e14
Facundo Gauna <fgauna12@gmail.com>
2020-10-06 07:21:30 -0700

    Update update-sealed-secret.sh

--- k8s/secrets/update-sealed-secret.sh
+++ k8s/secrets/update-sealed-secret.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+set -e
+
+SECRET_NAME=$1
+SEALED_SECRETS_FILENAME=$2
+SECRET_KEY=$3
+NEW_VALUE=$4
+
 echo -n "$NEW_VALUE" | kubectl create secret generic $SECRET_NAME --dry-run=client \
   --from-file=$SECRET_KEY=/dev/stdin -o yaml \
   | kubeseal --controller-name=sealed-secrets -o yaml --merge-into $SEALED_SECRETS_FILENAME
