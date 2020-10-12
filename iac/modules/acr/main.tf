9156e7c9d1ead52cb2ef5a1e0fb71bbbc51affd9
Facundo Gauna <fgauna12@gmail.com>
2020-10-12 13:37:10 -0700

    Starting new module for container registry

---
+++ iac/modules/acr/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = "~>2.30"
+    }
+  }
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.container_registry_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = var.container_registry_sku
+  admin_enabled       = true
+}
--- iac/modules/acr/variables.tf
+++ iac/modules/acr/variables.tf
@@ -3,7 +3,7 @@ variable "resource_group_name" {
 }
 
 variable "container_registry_name" {
-  
+
 }
 
 variable "container_registry_sku" {
